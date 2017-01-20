beb340857c24010cf3cc2310133047969af447f0
Francesco Mari <mari.francesco@gmail.com>
2017-01-20 10:24:23 +0100

    Move the format flag implementation in its own file

--- sdb.go
+++ sdb.go
@@ -56,41 +56,6 @@ func commands(reason string) {
 	fmt.Fprintf(os.Stderr, "    binaries    Print the content of a TAR binary index\n")
 }
 
-type format int
-
-const (
-	formatText format = iota
-	formatHex
-)
-
-const (
-	formatHexString  = "hex"
-	formatTextString = "text"
-)
-
-func (f *format) String() string {
-	switch *f {
-	case formatHex:
-		return formatHexString
-	case formatText:
-		return formatTextString
-	default:
-		panic("Invalid format")
-	}
-}
-
-func (f *format) Set(s string) error {
-	switch s {
-	case formatHexString:
-		*f = formatHex
-	case formatTextString:
-		*f = formatText
-	default:
-		return fmt.Errorf("Invalid format '%s'", s)
-	}
-	return nil
-}
-
 type tool struct {
 	flags *flag.FlagSet
 }
